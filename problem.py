e59b41ef032974ee22f5c1523bedce4bad1975a8
Fatima-Ezzahra Fettah <fatima-ezzahra.fettah@emines.um6p.ma>
2023-02-23 16:28:52 +0100

    secondcommit

---
+++ problem.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import os
+# -----------------------------------------------------------------------------
+# Training / testing data reader
+# -----------------------------------------------------------------------------
+
+
+def _read_data(path, type_):
+
+    fname = 'churn-data-{}.csv'.format(type_)
+    fp = os.path.join(path, 'data', fname)
+    data = pd.read_csv(fp)
+    col_names = list(data.columns)
+    col_names.remove('Churn')
+    features = data[col_names]
+    y = data['Churn']
+
+    # for the "quick-test" mode, use less data
+    test = os.getenv('RAMP_TEST_MODE', 0)
+    if test:
+        N_small = 35000
+        features = features[:N_small]
+        y = y[:N_small]
+
+    return features, y
+
+
+def get_train_data(path='.'):
+    return _read_data(path, 'train')
+
+
+def get_test_data(path='.'):
+    return _read_data(path, 'test')
+
+
